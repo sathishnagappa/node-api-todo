00434e01ea7c5124c1c4a5cb4b504cc9951d8e1a
sathish.nagappa <sathish.nagappa@brillio.com>
2018-03-20 20:03:05 +0530

    test get  api

--- server/server.js
+++ server/server.js
@@ -27,5 +27,12 @@ res.send(doc);
 });
 });
 
+app.get("/todos", (req, res) => {
+    Todo.find().then((todos) => {
+     res.send({todos});    
+    }, (e) => {
+      res.status(400).send(e);
+    });
+});
 
 module.exports = {app};
