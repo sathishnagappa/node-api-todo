038c02dbf8f79cafb92bebf346f2ff6defae2680
sathish.nagappa <sathish.nagappa@brillio.com>
2018-03-21 14:42:16 +0530

    Add test cases for get by id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require("expect");
 const request = require("supertest");
 
+const {ObjectID} = require("mongodb");
 const {app} = require("./../server");
 const {Todo} = require("./../models/todo");
 
 const todos = [{
+  "_id" : new ObjectID(),
  "text" : "This is test1"
 },{
+"_id" : new ObjectID(),
 "text" : "This is test1"
 }];
 
@@ -72,4 +75,28 @@ describe("GET/todos", () => {
         })
         .end(done);
     });
+});
+
+describe("GET/todos/:id", (done) => {
+it("should return todo doc", (done) => {
+  request(app)
+  .get("/todos/" + todos[0]._id.toHexString())
+  .expect(200)
+  .expect((res)=> {
+      expect(res.body.todo.text).toBe(todos[0].text);
+  })
+  .end(done)
+});
+it("should return 404 for todo not found", (done) => {
+    request(app)
+    .get("/todos/" + new ObjectID().toHexString())
+    .expect(404)
+    .end(done)
+});
+it("should return 404 for non-objects", (done) => {
+    request(app)
+    .get("/todos/123")
+    .expect(404)
+    .end(done)
+});
 });
