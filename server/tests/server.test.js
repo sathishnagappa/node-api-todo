c514ef0cb5fafe87af01c4955e7f66d7dc917e84
sathish.nagappa <sathish.nagappa@brillio.com>
2018-03-22 13:59:27 +0530

    Patch request test api

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
  "text" : "This is test1"
 },{
 "_id" : new ObjectID(),
-"text" : "This is test1"
+"text" : "This is test2",
+ "completed" : true,
+ "completedAt" : 333
 }];
 
 beforeEach((done) => {
@@ -133,4 +135,41 @@ describe("DELETE/todos/:id", (done) => {
         .expect(404)
         .end(done)
     });
+    });
+
+    describe("PATCH/todos", () => {
+        it("should update the todo", (done) => {
+        var text = "This is new test";
+        request(app)
+            .patch("/todos/" + todos[0]._id.toHexString())
+            .send({
+                text,
+                completed : true
+            })
+            .expect(200)
+            .expect((res)=> {
+                    expect(res.body.todo.text).toBe(text);
+                    expect(res.body.todo.completed).toBe(true);
+                    expect(res.body.todo.completedAt).toBeA("number");                    
+                })
+            .end(done)
+      
+        });
+
+        it("should clear completedAt when todo is not completed", (done) => {
+            var text = "This is new test";
+            request(app)
+            .patch("/todos/" + todos[1]._id.toHexString())
+            .send({
+                text,
+                completed : false
+            })
+            .expect(200)
+            .expect((res)=> {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toNotExist();
+             })
+             .end(done)
+        });
     });
