29057c706a6c3af82bacefa4447d7ca5217e1b15
sathish.nagappa <sathish.nagappa@brillio.com>
2018-04-12 15:59:43 +0530

    =expect upgrade

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -120,7 +120,7 @@ describe("DELETE/todos/:id", (done) => {
             }
 
             Todo.findById(todos[1]._id.toHexString()).then((todo) => {
-                expect(todo).toNotExist();
+                expect(todo).toBeFalsy();
                 done();
             }).catch((e) => done(e) );
             
@@ -136,7 +136,7 @@ describe("DELETE/todos/:id", (done) => {
                    return done(err);   
               }  
               Todo.findById(todos[1]._id.toHexString()).then((todo) => {
-                  expect(todo).toExist();
+                  expect(todo).toBeTruthy();
                   done();
               }).catch((e) => done(e) );
               
@@ -172,7 +172,8 @@ describe("DELETE/todos/:id", (done) => {
             .expect((res)=> {
                     expect(res.body.todo.text).toBe(text);
                     expect(res.body.todo.completed).toBe(true);
-                    expect(res.body.todo.completedAt).toBeA("number");                    
+                    // expect(res.body.todo.completedAt).toBeA("number");                    
+                    expect(typeof res.body.todo.completedAt).toBe("number");
                 })
             .end(done)
       
@@ -204,7 +205,7 @@ describe("DELETE/todos/:id", (done) => {
             .expect((res)=> {
                 expect(res.body.todo.text).toBe(text);
                 expect(res.body.todo.completed).toBe(false);
-                expect(res.body.todo.completedAt).toNotExist();
+                expect(res.body.todo.completedAt).toBeFalsy();
              })
              .end(done)
         });
@@ -244,8 +245,8 @@ describe("DELETE/todos/:id", (done) => {
             .send({email, password})
             .expect(200)
             .expect((res) =>  {
-                expect(res.header["x-auth"]).toExist();    
-                expect(res.body._id).toExist();
+                expect(res.header["x-auth"]).toBeTruthy();    
+                expect(res.body._id).toBeTruthy();
                 expect(res.body.email).toBe(email);
             })
             .end((err) => {
@@ -254,8 +255,8 @@ describe("DELETE/todos/:id", (done) => {
                 }
                
                 Users.findOne({email}).then((user) => {
-                   expect(user).toExist();
-                   expect(user.password).toNotBe(password);
+                   expect(user).toBeTruthy();
+                   expect(user.password).not.toBe(password);
                    done();    
                 }).catch((e) => done());
             })    
@@ -278,9 +279,9 @@ describe("DELETE/todos/:id", (done) => {
             .send({email, password})
             .expect(400)
             .expect((res) =>  {
-                expect(res.header["x-auth"]).toNotExist();    
-                expect(res.body._id).toNotExist();
-                expect(res.body.email).toNotExist(email);
+                expect(res.header["x-auth"]).toBeFalsy();    
+                expect(res.body._id).toBeFalsy();
+                expect(res.body.email).toBeFalsy();
             })
             .end(done)               
             });
@@ -296,14 +297,14 @@ describe("DELETE/todos/:id", (done) => {
             })
             .expect(200)
             .expect((res) => {
-                expect(res.header["x-auth"]).toExist();
+                expect(res.header["x-auth"]).toBeTruthy();
             })
             .end((err,res) => {
                 if(err) {
                     return done(err);
                 }
                 Users.findById(users[1]._id).then((user) => {
-                    expect(user[0].tokens[1]).toInclude({
+                    expect(user[0].toObject().tokens[1]).toMatchObject({
                         access : "auth",
                         token : res.header["x-auth"]
                     });
@@ -320,7 +321,7 @@ describe("DELETE/todos/:id", (done) => {
             })
             .expect(400)
             .expect((res) => {
-                expect(res.header["x-auth"]).toNotExist();
+                expect(res.header["x-auth"]).toBeFalsy();
             })
             .end((err,res) => {
                 if(err) {
